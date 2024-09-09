ee1489e10586f5f3735d7dca39ec421d33a8be2c
Asliddin Amirov <asliddinamir2@gmail.com>
2024-09-09 09:42:06 +0800

    last commit

--- tajik_essays/lib/AdHelper/ad_helper.dart
+++ tajik_essays/lib/AdHelper/ad_helper.dart
@@ -1,37 +1,3 @@
-// import 'dart:io';
-
-// class AdHelper {
-//   static String get bannerAdUnitId {
-//     if (Platform.isAndroid) {
-//       return 'ca-app-pub-3040413046352040/6444720585'; // Replace with your Banner Ad Unit ID for Android
-//     } else if (Platform.isIOS) {
-//       return 'ca-app-pub-3040413046352040/8579123436'; // Replace with your Banner Ad Unit ID for iOS
-//     } else {
-//       throw UnsupportedError('Unsupported platform');
-//     }
-//   }
-
-//   static String get interstitialAdUnitId {
-//     if (Platform.isAndroid) {
-//       return 'ca-app-pub-3040413046352040/5952960093'; // Replace with your Interstitial Ad Unit ID for Android
-//     } else if (Platform.isIOS) {
-//       return 'ca-app-pub-3040413046352040/9334607801'; // Replace with your Interstitial Ad Unit ID for iOS
-//     } else {
-//       throw UnsupportedError('Unsupported platform');
-//     }
-//   }
-
-//   static String get nativeAdUnitId {
-//     if (Platform.isAndroid) {
-//       return 'ca-app-pub-3040413046352040/6181830856'; // Replace with your Native Ad Unit ID for Android
-//     } else if (Platform.isIOS) {
-//       return 'ca-app-pub-3040413046352040/9333801909'; // Replace with your Native Ad Unit ID for iOS
-//     } else {
-//       throw UnsupportedError('Unsupported platform');
-//     }
-//   }
-// }
-
 import 'dart:io';
 // import 'package:google_mobile_ads/google_mobile_ads.dart';
 
