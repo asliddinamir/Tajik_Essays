89474c30e71775fa539e68f00ff8722dd2188aed
Asliddin Amirov <asliddinamir2@gmail.com>
2024-09-11 09:16:57 +0800

    working

--- tajik_essays/lib/AdHelper/ad_helper.dart
+++ tajik_essays/lib/AdHelper/ad_helper.dart
@@ -1,16 +1,16 @@
-import 'dart:io';
-// import 'package:google_mobile_ads/google_mobile_ads.dart';
+// import 'dart:io';
+// // import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-class AdHelper {
-  static String get bannerAdUnitId {
-    return Platform.isAndroid
-        ? 'ca-app-pub-3940256099942544/6300978111' // Replace with your banner ad unit ID
-        : 'ca-app-pub-3940256099942544/2934735716'; // Replace with your banner ad unit ID
-  }
+// class AdHelper {
+//   static String get bannerAdUnitId {
+//     return Platform.isAndroid
+//         ? 'ca-app-pub-3940256099942544/6300978111' // Replace with your banner ad unit ID
+//         : 'ca-app-pub-3940256099942544/2934735716'; // Replace with your banner ad unit ID
+//   }
 
-  static String get interstitialAdUnitId {
-    return Platform.isAndroid
-        ? 'ca-app-pub-3940256099942544/1033173712' // Replace with your interstitial ad unit ID
-        : 'ca-app-pub-3940256099942544/4411468910'; // Replace with your interstitial ad unit ID
-  }
-}
+//   static String get interstitialAdUnitId {
+//     return Platform.isAndroid
+//         ? 'ca-app-pub-3940256099942544/1033173712' // Replace with your interstitial ad unit ID
+//         : 'ca-app-pub-3940256099942544/4411468910'; // Replace with your interstitial ad unit ID
+//   }
+// }
